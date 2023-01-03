7e4466dd02ab626175f86012df3f6929b7e9f029
annuszka <annrybak92@gmail.com>
2023-01-03 14:03:34 +0100

    Added toString, equals and hashcode methods to user class

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,8 +4,11 @@ public class MainApp {
     public static void main(String[] args) {
 
         User user1 = new User("Laura", "Palmer", "laura@twinpeaks.com", 18 );
+        User user2 = new User("Laura", "Palmer", "laura@twinpeaks.com", 18 );
 
         System.out.println(user1.isUserAdult());
         System.out.println(User.getUserCounter());
+        System.out.println(user1);
+        System.out.println(user1.equals(user2));
     }
 }
